8bebae025476369c3d3ac1ade8607af9c52d44b2
linty <linty@meitu.com>
2017-11-14 18:08:28 +0800

    MOD: Refactor variable

--- src/nc_process.c
+++ src/nc_process.c
@@ -254,13 +254,13 @@ nc_worker_process(int worker_id, struct instance *nci)
     // TODO: worker should remove the listening sockets from event base and after lingering connections are exhausted
     // or timeout, quit process.
 
-    bool has_pre_terminated = false;
+    bool terminating = false;
     for (;!pm_quit;) {
-        if (pm_terminate && !has_pre_terminated) {
+        if (pm_terminate && !terminating) {
             // close proxy listen fd, and wait for 30 seconds
             proxy_deinit(nci->ctx);
             nc_set_timer(30000, 0);
-            has_pre_terminated = true;
+            terminating = true;
         }
         status = core_loop(nci->ctx);
         if (status != NC_OK) {
