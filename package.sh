8d28c311420850abf4a504e95cec4192ca27b71b
Wenhao Huang <hwh@meitu.com>
2017-12-05 18:36:06 +0800

    FIX: Add sysv init script and logrotate

--- package.sh
+++ package.sh
@@ -4,8 +4,17 @@ NAME="twemproxy"
 VERSION=`head -1 .version|awk -F- '{printf substr($2, 0, length($2)-1);}'`
 rm .version
 STAGE=${STAGE:-release}
-fpm -f -s dir -t rpm --prefix '/www/twemproxy'  -n ${NAME} --epoch 7 \
+
+# setup fake root
+mkdir -p _root/www/twemproxy/logs
+mkdir -p _root/etc/logrotate.d
+cp -r _build/* _root/www/twemproxy/
+cp rpm/logrotate _root/etc/logrotate.d/twemproxy
+
+fpm -f -s dir -t rpm -n ${NAME} --epoch 7 -C _root \
     --config-files /www/twemproxy/conf/nutcracker.yml \
-    -v ${VERSION} --iteration ${CI_PIPELINE_ID}.${STAGE} -C ./_build \
+    -v ${VERSION} --iteration ${CI_PIPELINE_ID}.${STAGE} \
     --verbose --category 'Meitu/Projects' --description 'twemproxy' \
-    --url 'http://www.meitu.com' --license 'Commerial' -m 'linty@meitu.com'
+    --url 'http://www.meitu.com' --license 'Commerial' -m 'linty@meitu.com' \
+    --rpm-init ./rpm/twemproxy.init --after-install ./rpm/after-install --before-remove ./rpm/before-remove \
+    www/twemproxy etc/logrotate.d/twemproxy
