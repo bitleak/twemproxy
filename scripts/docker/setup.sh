9c893b687144ae38d72e6533835a3d0659894acb
huanglin <huanglin@meitu.com>
2017-12-05 17:53:44 +0800

    MOD: Always build twemproxy image before test

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -2,4 +2,5 @@
 branch=`git rev-parse --abbrev-ref HEAD`
 cd ../../ && git archive ${branch} --format tar.gz -o twemproxy.tar.gz
 mv twemproxy.tar.gz scripts/docker/twemproxy && cd -
+docker-compose -p twemproxy build
 docker-compose -p twemproxy up -d --force-recreate
