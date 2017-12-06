012045d654de58d2ed34494ee5bf1e48078d46eb
huanglin <huanglin@meitu.com>
2017-12-06 09:50:42 +0800

    TST: Add test case for renamenx command

--- tests/test_redis/test_rename.py
+++ tests/test_redis/test_rename.py
@@ -1,14 +1,18 @@
 from common import *
 
 def test_rename_in_master_slave():
-    nc = redis.Redis(nc_servers['redis-ms']['host'], nc_servers['redis-ms']['port'])
+    nc = get_redis_conn(is_ms=True)
     nc.execute_command('AUTH', redis_passwd)
     nc.delete('new_key')
     nc.set('rename-key', '1')
     assert(nc.rename('rename-key', 'new_key'))
+    nc.delete('new_key_2')
+    assert(nc.renamenx('new_key', 'new_key_2'))
 
 def test_rename_in_shards():
-    nc = redis.Redis(nc_servers['redis-shards']['host'], nc_servers['redis-shards']['port'])
+    nc = get_redis_conn(is_ms=False)
     nc.delete('new_key')
     nc.set('rename-key', '1')
     assert_fail('unknown command', nc.rename,'rename-key', 'new_key')
+    nc.delete('new_key')
+    assert_fail('unknown command', nc.renamenx, 'new_key', 'new_key_2')
